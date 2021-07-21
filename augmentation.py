1b88ba11458082894dc34475516ba1f2d9efa4ec
Fabio Rose <rosef@hochschule-trier.de>
2021-07-22 01:04:29 +0200

    added data preprocessing and generator

---
+++ augmentation.py
@@ -0,0 +1,119 @@
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+from math import cos, sin, pi
+from groundtruth import map_coco_to_personlab
+
+flip_prob = 0.5
+max_degree = 30.
+max_scale = 2.
+min_scale = 0.8
+max_offset = 20.0
+target_dist = 0.8
+scale_prob = 1.
+IMAGE_SHAPE = (416, 416, 3)
+RIGHT_KP = [1, 2, 3,  7,  8,  9, 13, 15]
+LEFT_KP = [4, 5, 6, 10, 11, 12, 14, 16]
+
+
+class AugmentationSelection:
+    def __init__(self, flip=False, degree=0., crop=(0, 0), scale=1.):
+        self.flip = flip
+        self.degree = degree
+        self.crop = crop
+        self.scale = scale
+
+    @staticmethod
+    def random():
+        flip = np.random.uniform(0., 1.) > flip_prob   # Flip with probability of 50%
+        degree = np.random.uniform(-1., 1.) * max_degree  # Rotate between -30° and 30°
+        scale = (max_scale - min_scale) * np.random.uniform(0., 1.) + min_scale if np.random.uniform(0., 1.) < \
+            scale_prob else 1.# Scale factor between 0.8 and 2.0
+        x_offset = int(np.random.uniform(-1., 1.) * max_offset)  # Offset between -20px and 20px
+        y_offset = int(np.random.uniform(-1., 1.) * max_offset)  # Offset between -20px and 20px
+
+        return AugmentationSelection(flip, degree, (x_offset, y_offset), scale)
+
+    @staticmethod
+    def unrandom():
+        flip = False
+        degree = 0.
+        scale = 1.
+        x_offset = 0
+        y_offset = 0
+        return AugmentationSelection(flip, degree, (x_offset, y_offset), scale)
+
+    def affine(self, center=(IMAGE_SHAPE[1]//2, IMAGE_SHAPE[0]//2)):
+        A = self.scale * cos(self.degree / 180. * pi)
+        B = self.scale * sin(self.degree / 180. * pi)
+
+        scale_size = target_dist / self.scale
+
+        (width, height) = center
+        center_x = width + self.crop[0]
+        center_y = height + self.crop[1]
+
+        center2zero = np.array([[1., 0., -center_x],
+                                [0., 1., -center_y],
+                                [0., 0., 1.]])
+
+        rotate = np.array([[A, B, 0],
+                           [-B, A, 0],
+                           [0, 0, 1.]])
+
+        scale = np.array([[scale_size, 0, 0],
+                          [0, scale_size, 0],
+                          [0, 0, 1.]])
+
+        flip = np.array([[-1 if self.flip else 1., 0., 0.],
+                         [0., 1., 0.],
+                         [0., 0., 1.]])
+
+        center2center = np.array([[1., 0., IMAGE_SHAPE[1] // 2],
+                                  [0., 1., IMAGE_SHAPE[0] // 2],
+                                  [0., 0., 1.]])
+
+        combined = center2center.dot(flip).dot(scale).dot(rotate).dot(center2zero)
+        return combined[0:2]
+
+
+class Transformer:
+    @staticmethod
+    def transform(img, masks, keypoints, aug=AugmentationSelection.random()):
+
+        # warp picture and mask
+        M = aug.affine(center=(img.shape[1] // 2, img.shape[0] // 2))
+        cv_shape = (IMAGE_SHAPE[1], IMAGE_SHAPE[0])
+
+        img = cv2.warpAffine(img, M, cv_shape, flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_CONSTANT,
+                             borderValue=(127, 127, 127))
+        out_masks = np.zeros(cv_shape[::-1] + (masks.shape[-1],))
+        for i in range(masks.shape[-1]):
+            out_masks[:, :, i] = cv2.warpAffine(masks[:, :, i], M, cv_shape, flags=cv2.INTER_CUBIC,
+                                                borderMode=cv2.BORDER_CONSTANT, borderValue=0)
+        masks = out_masks
+
+        # warp key points
+        keypoints = map_coco_to_personlab(keypoints)
+        original_points = keypoints.copy()
+        # print keypoints
+        original_points[:, :, 2] = 1  # we reuse 3rd column in completely different way here, it is hack
+        converted_points = np.matmul(M, original_points.transpose([0, 2, 1])).transpose([0, 2, 1])
+        keypoints[:, :, 0:2] = converted_points
+
+        cropped_kp = keypoints[:, :, 0] >= IMAGE_SHAPE[1]
+        cropped_kp = np.logical_or(cropped_kp, keypoints[:, :, 1] >= IMAGE_SHAPE[0])
+        cropped_kp = np.logical_or(cropped_kp, keypoints[:, :, 0] < 0)
+        cropped_kp = np.logical_or(cropped_kp, keypoints[:, :, 1] < 0)
+
+        keypoints[cropped_kp, 2] = 0
+
+        # we just made image flip, i.e. right leg just became left leg, and vice versa
+        if aug.flip:
+            tmpLeft = keypoints[:, LEFT_KP, :]
+            tmpRight = keypoints[:, RIGHT_KP, :]
+            keypoints[:, LEFT_KP, :] = tmpRight
+            keypoints[:, RIGHT_KP, :] = tmpLeft
+
+        # print keypoints
+        return img, masks, keypoints
---
+++ data_generator.py
@@ -0,0 +1,123 @@
+from pycocotools.coco import COCO
+import numpy as np
+import os
+import cv2
+from augmentation import AugmentationSelection, Transformer
+from groundtruth import get_ground_truth
+
+# Path of Annotation file
+ANNO_FILE = "./coco2017/annotations/person_keypoints_train2017.json"
+IMG_DIR = "./coco2017/train2017"
+NUM_KP = 17
+IMAGE_SHAPE = (416, 416, 3)
+NUM_EDGES = 16
+
+
+class DataGenerator(object):
+    def __init__(self):
+        self.coco = COCO(ANNO_FILE)
+        self.img_ids = list(self.coco.imgs.keys())
+        self.datasetlen = len(self.img_ids)
+        self.id = 0
+
+    def get_one_sample(self, give_id=None, is_aug=True):
+        if self.id == self.datasetlen:
+            self.id = 0
+        if give_id is None:
+            img_id = self.img_ids[self.id]
+        else:
+            img_id = give_id
+        filepath = os.path.join(IMG_DIR, self.coco.imgs[img_id]["file_name"])
+        img = cv2.imread(filepath)
+        h, w, c = img.shape
+        crowd_mask = np.zeros((h, w), dtype="bool")
+        unannotated_mask = np.zeros((h, w), dtype="bool")
+        instance_masks = []
+        keypoints = []
+        img_anns = self.coco.loadAnns(self.coco.getAnnIds(imgIds=img_id))
+
+        for anno in img_anns:
+            # if crowd, don't compute loss
+            mask = self.coco.annToMask(anno)
+            if anno['iscrowd'] == 1:
+                crowd_mask = np.logical_or(crowd_mask, mask)
+            # if tiny instance, don't compute loss
+            elif anno['num_keypoints'] == 0:
+                unannotated_mask = np.logical_or(unannotated_mask, mask)
+                instance_masks.append(mask)
+                keypoints.append(anno['keypoints'])
+            else:
+                instance_masks.append(mask)
+                keypoints.append(anno['keypoints'])
+        if len(instance_masks) <= 0:
+            self.id += 1
+            return None
+
+        kp = np.reshape(keypoints, (-1, NUM_KP, 3))
+        instance_masks = np.stack(instance_masks).transpose((1, 2, 0))
+        overlap_mask = instance_masks.sum(axis=-1) > 1
+        seg_mask = np.logical_or(crowd_mask, np.sum(instance_masks, axis=-1))
+
+        # Augmentation
+        single_masks = [seg_mask, unannotated_mask, crowd_mask, overlap_mask]
+        all_masks = np.concatenate([np.stack(single_masks, axis=-1), instance_masks], axis=-1)
+        if is_aug:
+            aug = AugmentationSelection.random()
+        else:
+            aug = AugmentationSelection.unrandom()
+        img, all_masks, kp = Transformer.transform(img, all_masks, kp, aug=aug)
+
+        num_instances = instance_masks.shape[-1]
+        instance_masks = all_masks[:, :, -num_instances:]
+        seg_mask, unannotated_mask, crowd_mask, overlap_mask = all_masks[:, :, :4].transpose((2, 0, 1))
+        seg_mask, unannotated_mask, crowd_mask, overlap_mask = [np.expand_dims(m, axis=-1) for m in [seg_mask,
+                                                                                                     unannotated_mask,
+                                                                                                     crowd_mask,
+                                                                                                     overlap_mask]]
+
+        unannotated_mask = np.logical_not(unannotated_mask)
+        crowd_mask = np.logical_not(crowd_mask)
+        overlap_mask = np.logical_not(overlap_mask)
+
+        kp = [np.squeeze(k) for k in np.split(kp, kp.shape[0], axis=0)]
+        kp_maps, short_offsets, mid_offsets, long_offsets = get_ground_truth(instance_masks, kp)
+        self.id += 1
+        return [img.astype('float32') / 255.0, kp_maps.astype('float32'), short_offsets.astype('float32'),
+                mid_offsets.astype('float32'), long_offsets.astype('float32'), seg_mask.astype('float32'),
+                crowd_mask.astype('float32'), unannotated_mask.astype('float32'), overlap_mask.astype('float32')]
+
+    def gen_batch(self, batch_size=4):
+        h, w, c = IMAGE_SHAPE
+        while True:
+            imgs_batch = np.zeros((batch_size, h, w, c))
+            kp_maps_batch = np.zeros((batch_size, h, w, NUM_KP))
+            short_offsets_batch = np.zeros((batch_size, h, w, 2 * NUM_KP))
+            mid_offsets_batch = np.zeros((batch_size, h, w, 4 * NUM_EDGES))
+            long_offsets_batch = np.zeros((batch_size, h, w, 2 * NUM_KP))
+            seg_mask_batch = np.zeros((batch_size, h, w, 1))
+            crowd_mask_batch = np.zeros((batch_size, h, w, 1))
+            unannotated_mask_batch = np.zeros((batch_size, h, w, 1))
+            overlap_mask_batch = np.zeros((batch_size, h, w, 1))
+
+            for i in range(batch_size):
+                sample = self.get_one_sample()
+                while sample is None:  # not to train the images with no instance
+                    sample = self.get_one_sample()
+                imgs_batch[i] = sample[0]
+                kp_maps_batch[i] = sample[1]
+                short_offsets_batch[i] = sample[2]
+                mid_offsets_batch[i] = sample[3]
+                long_offsets_batch[i] = sample[4]
+                seg_mask_batch[i] = sample[5]
+                crowd_mask_batch[i] = sample[6]
+                unannotated_mask_batch[i] = sample[7]
+                overlap_mask_batch[i] = sample[8]
+
+            yield [imgs_batch, kp_maps_batch, short_offsets_batch, mid_offsets_batch, long_offsets_batch,
+                   seg_mask_batch, crowd_mask_batch, unannotated_mask_batch, overlap_mask_batch]
+
+
+datagen = DataGenerator()
+
+for el in datagen.gen_batch():
+    print(len(el[0]))
